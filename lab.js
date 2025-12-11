425140d80b6ba5ac8eb6f802aa1cdc52aa5e437c
IsaacStearn <113034507+BlitzoSupreme@users.noreply.github.com>
2025-12-11 13:44:20 +0000

    Update lab.js

--- lab.js
+++ lab.js
@@ -1,4 +1,6 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -219,6 +221,11 @@ function onWindowResize(){
 }
 
 window.addEventListener('resize', onWindowResize);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.05;
+controls.autoRotate = false;
 
 
 //create skybox function
